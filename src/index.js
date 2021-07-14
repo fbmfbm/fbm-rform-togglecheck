5c29f7c300dbcfb5aededd83bc7c014a74d7c63a
fbmfbm <fbmaison@gmail.com>
2021-07-14 13:22:34 +0200

    DEV: initial coimmit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ToggleCheck from './lib/ToggleCheck';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>Test fbm react form ToggleCheck input</div>
+    <ToggleCheck />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/lib/ToggleCheck.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./ToggleCheck.css"
+
+const ToggleCheck = (props) => {
+   return (
+       <div className="fbm-rform-input">Toggle check !!</div>
+   )
+}
+
+export default ToggleCheck
+
